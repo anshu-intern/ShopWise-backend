aeee44b0d0823a85c412d3d5fc374176e240a793
anshu-intern <anshumanik00@gmail.com>
2025-11-08 18:19:20 +0530

    Removed emv config

--- Middleware/storage.cloudinary.js
+++ Middleware/storage.cloudinary.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
+//import dotenv from 'dotenv';
+//dotenv.config();
 
 import cloudinary from 'cloudinary';
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // load environment variables
-import dotenv from 'dotenv';
-dotenv.config();
+//import dotenv from 'dotenv';
+//dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
